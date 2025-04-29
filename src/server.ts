bcdbb2992b5814713c68aba074b454f3783f7727
Kevin Mistrele <kevintavaresmistrele@gmail.com>
2025-04-29 16:04:41 -0300

    feat: adding start command

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,9 @@ dotenv.config()
 connectDB()
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: process.env.FRONTEND_URL || '*',
+}))
 app.use(express.json())
 
 app.use('/api', authRoutes)
@@ -18,6 +20,7 @@ app.get('/', (_req, res) => {
   res.send('API funcionando!')
 })
 
-app.listen(3001, () => {
-  console.log('Servidor rodando na porta 3001')
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
